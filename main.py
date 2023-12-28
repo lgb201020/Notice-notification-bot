1ceb0d7cccc91259601fd357b95dd313a622dd10
lgb201020 <pssdjl123@naver.com>
2023-12-28 17:56:18 +0900

    공지사항 알림 봇 코드 작성 및 알림 코드 작성

---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import time
+
+
+URL = "https://eei.jbnu.ac.kr/eei/10634/subview.do?enc=Zm5jdDF8QEB8JTJGYmJzJTJGZWVpJTJGMTEwMCUyRmFydGNsTGlzdC5kbyUzRg%3D%3D"
+mytoken = "xoxb-6386561975127-6423902697472-OJ0cgqSYYFqXMOTiGj9ZtOV9"
+
+def to_find_notice(url):
+    res =  requests.get(url)
+    res.raise_for_status()
+    soup = BeautifulSoup(res.text, "lxml")
+    """
+    웹 서버로부터 응답 받은 객체 중에서 text 속성(HTML 소스 코드)을 선택하고, 
+    우리가 받은 HTML 소스 코드를 lxml 파서를 통해 BeautifulSoup 객체로 만든다(응답받은 데이터를 soup 변수에 할당한다)
+    """
+
+    to_find_notice2 = soup.findAll("td", attrs={"class" : "_artclTdRdate"})
+    notice_date = []
+    for nt in to_find_notice2:
+        strdata_of_date = str(list(nt)[0])
+        date_data = datetime.strptime(strdata_of_date, "%Y.%m.%d")
+        notice_date.append(date_data)
+    return sorted(notice_date, reverse=True)
+
+
+def post_message(token, channel, text):
+    """슬랙 메시지 전송"""
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+
+
+
+while True:
+    try:
+        if to_find_notice(URL)[0]==datetime.today():
+            text_notification = "전자공 공지사항에 새로운 공지글이 올라왔어요."
+            post_message(mytoken,"#notice_notification",text_notification )
+        else:
+            time.sleep(3600)
+            
+    except Exception as e:
+        print(e)
+        text_error = "오류가 발생했어요."
+        post_message(mytoken,"#notice_notification",text_error )
+        time.sleep(1)
